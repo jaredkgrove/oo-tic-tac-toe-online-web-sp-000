a4a06860d6e8e7e5215732818ea3bd572c775182
Jared Grove <jaredkgrove@gmail.com>
2019-03-11 00:43:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,17 +20,13 @@ class TicTacToe
   def move
     @board[@index] = @mark
   end
+  
+  def position_taken?
+    !(@board[@index].nil? || @board[@index] == " ")
+  end
 
-end
-
-
-
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?
+  @index.between?(0,8) && !position_taken?
 end
 
 def turn(board)
@@ -87,4 +83,11 @@ def play(board)
     puts "Cat's Game!"
   end
 end
+end
+
+
+
+
+
+
 
